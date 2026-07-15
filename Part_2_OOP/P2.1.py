aef96f1431fd2a9d732ed778c1c435831c7aeb76
Yousif-hash020 <arainyousif020@gmail.com>
2026-07-15 23:35:17 +0500

    commit

--- Part_2_OOP/P2.1.py
+++ Part_2_OOP/P2.1.py
@@ -8,3 +8,17 @@ student1 = Student('Yousaf', 158)
 
 print(student1.x)
 print(student1.y)
+
+
+class Car:
+    def __init__(self, brand, color = 'Black'):
+
+        self.brand = brand
+        self.color = color
+
+        
+
+new_Car = Car('toyita')
+
+print(new_Car.color)
+print(new_Car.brand)        
