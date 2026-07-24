9a7587124e9ff876382eb46a49e836c0ea62dc59
Yousif-hash020 <arainyousif020@gmail.com>
2026-07-25 01:10:55 +0500

    Selection_sort

---
+++ Sorting/Selection_sort.py
@@ -0,0 +1,13 @@
+def Selection_Sort():
+    arr = [5,2,4,1,1]
+
+    for i in range(len(arr)):
+        min = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min]:
+                min = j
+
+        arr[i], arr[min] = arr[min], arr[i]
+    print(arr)
+
+Selection_Sort()
